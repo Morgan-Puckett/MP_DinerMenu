76479f64ac42fd66bcb75ac6049f6e8456e3d0cf
Morgan Puckett <morganpuckett02@gmail.com>
2022-03-21 15:31:33 -0600

    Add project files.

---
+++ MP_DinerMenu.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MP_DinerMenu
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.SoupButton = New System.Windows.Forms.Button()
+        Me.SaladButton = New System.Windows.Forms.Button()
+        Me.FishButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.DinerNameLabel = New System.Windows.Forms.Label()
+        Me.DinerSpecialtyLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'SoupButton
+        '
+        Me.SoupButton.Location = New System.Drawing.Point(12, 429)
+        Me.SoupButton.Name = "SoupButton"
+        Me.SoupButton.Size = New System.Drawing.Size(113, 90)
+        Me.SoupButton.TabIndex = 0
+        Me.SoupButton.Text = "Soup"
+        Me.SoupButton.UseVisualStyleBackColor = True
+        '
+        'SaladButton
+        '
+        Me.SaladButton.Location = New System.Drawing.Point(131, 429)
+        Me.SaladButton.Name = "SaladButton"
+        Me.SaladButton.Size = New System.Drawing.Size(113, 90)
+        Me.SaladButton.TabIndex = 1
+        Me.SaladButton.Text = "Salad"
+        Me.SaladButton.UseVisualStyleBackColor = True
+        '
+        'FishButton
+        '
+        Me.FishButton.Location = New System.Drawing.Point(250, 429)
+        Me.FishButton.Name = "FishButton"
+        Me.FishButton.Size = New System.Drawing.Size(113, 90)
+        Me.FishButton.TabIndex = 2
+        Me.FishButton.Text = "Fish"
+        Me.FishButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(670, 429)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(113, 90)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "Exit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'DinerNameLabel
+        '
+        Me.DinerNameLabel.AutoSize = True
+        Me.DinerNameLabel.Font = New System.Drawing.Font("Mistral", 28.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.DinerNameLabel.Location = New System.Drawing.Point(277, 56)
+        Me.DinerNameLabel.Name = "DinerNameLabel"
+        Me.DinerNameLabel.Size = New System.Drawing.Size(206, 56)
+        Me.DinerNameLabel.TabIndex = 4
+        Me.DinerNameLabel.Text = "Moe's Diner"
+        '
+        'DinerSpecialtyLabel
+        '
+        Me.DinerSpecialtyLabel.AutoSize = True
+        Me.DinerSpecialtyLabel.Font = New System.Drawing.Font("Mistral", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.DinerSpecialtyLabel.Location = New System.Drawing.Point(44, 118)
+        Me.DinerSpecialtyLabel.Name = "DinerSpecialtyLabel"
+        Me.DinerSpecialtyLabel.Size = New System.Drawing.Size(25, 39)
+        Me.DinerSpecialtyLabel.TabIndex = 5
+        Me.DinerSpecialtyLabel.Text = " "
+        Me.DinerSpecialtyLabel.TextAlign = System.Drawing.ContentAlignment.BottomCenter
+        '
+        'MP_DinerMenu
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(795, 531)
+        Me.Controls.Add(Me.DinerSpecialtyLabel)
+        Me.Controls.Add(Me.DinerNameLabel)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.FishButton)
+        Me.Controls.Add(Me.SaladButton)
+        Me.Controls.Add(Me.SoupButton)
+        Me.Name = "MP_DinerMenu"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SoupButton As Button
+    Friend WithEvents SaladButton As Button
+    Friend WithEvents FishButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents DinerNameLabel As Label
+    Friend WithEvents DinerSpecialtyLabel As Label
+End Class
---
+++ MP_DinerMenu.vb
@@ -0,0 +1,38 @@
+﻿Public Class MP_DinerMenu
+    Private Sub FishButton_Click(sender As Object, e As EventArgs) Handles FishButton.Click
+        DinerSpecialtyLabel.Text = "~ Fish ~ 
+Tonight's Fish Special is a one pan lemon garlic salmon with 
+charred bell peppers on the side"
+    End Sub
+
+    Private Sub SaladButton_Click(sender As Object, e As EventArgs) Handles SaladButton.Click
+        DinerSpecialtyLabel.Text = "~ Salad ~
+Tonights Salad Special is a shredded chicken salad from my 
+pantry. Chicken didnt come from a can*
+
+
+
+*It totally did."
+    End Sub
+
+    Private Sub SoupButton_Click(sender As Object, e As EventArgs) Handles SoupButton.Click
+        DinerSpecialtyLabel.Text = "~ Soup ~
+The Soup Special this evening is a true delicacy, made from our
+finest ingredients. All the way from the 
+dollar tree, progresso soup of your choice 
+for only $559.99! We spent all our money on the 
+salmon and we aren't sorry!!!"
+    End Sub
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub MP_DinerMenu_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "Diner food mmmmmmm yummy"
+    End Sub
+End Class
+
+
+
+
